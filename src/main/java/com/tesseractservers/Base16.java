901176206d12f42c8fc562e65bcedda2ba000dfc
Tassilo <tassia710@gmail.com>
2021-02-02 12:14:50 +0100

    Create Base16.java

---
+++ src/main/java/com/tesseractservers/Base16.java
@@ -0,0 +1,70 @@
+package com.tesseractservers;
+
+/**
+ * Allows to convert byte arrays to Base16 (hexadecimal).
+ * @since Core 1.0
+ * @author Tassilo
+ */
+public class Base16 {
+
+	/**
+	 * This array is solely used to encode.
+	 */
+	private static final char[] TABLE = {
+		'0', '1', '2', '3', '4', '5', '6', '7',
+		'8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+	};
+
+	/**
+	 * This method is used to decode a character into its integer value (range: 0-15)
+	 * @param value the character to decode
+	 * @return the integer value
+	 */
+	private static int decodeChar(char value) {
+		if (value >= '0' && value <= '9') {
+			return value - '0';
+		} else if (value >= 'a' && value <= 'f') {
+			return value - 'a' + 10;
+		} else throw new IllegalArgumentException("Unknown character: " + value);
+	}
+
+	/**
+	 * Encodes a byte array into a Base16 string.
+	 * @param input the byte array to encode
+	 * @return the encoded string
+	 * @see #decode(String)
+	 */
+	public static String encode(byte[] input) {
+		char[] chars = new char[input.length * 2];
+		for (int i = 0; i < input.length; i++) {
+			chars[i * 2] = TABLE[(input[i] >> 4) & 0xf];
+			chars[i * 2 + 1] = TABLE[input[i] & 0xf];
+		}
+		return new String(chars);
+	}
+
+	/**
+	 * Decodes an input Base16 string.
+	 * @param input the string to decode
+	 * @return the decoded byte array
+	 * @see #encode(byte[])
+	 */
+	public static byte[] decode(String input) {
+		char[] chars = input.toCharArray();
+		if (chars.length % 2 != 0) throw new IllegalArgumentException("Strings length must be a multiple of 2.");
+		byte[] bytes = new byte[chars.length / 2];
+		for (int i = 0; i < bytes.length; i++) {
+			int a = decodeChar(chars[i * 2]);
+			int b = decodeChar(chars[i * 2 + 1]);
+			bytes[i] = (byte) (((a << 4) & 0xf0) | (b & 0xf));
+		}
+		return bytes;
+	}
+
+	/**
+	 * Static class.
+	 */
+	private Base16() {
+	}
+
+}
