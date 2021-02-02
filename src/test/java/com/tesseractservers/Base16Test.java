6fb9a9d7c79db7056c14ff8f098bbb73346f3511
Tassilo <tassia710@gmail.com>
2021-02-02 13:08:48 +0100

    Create Base16Test.java

---
+++ src/test/java/com/tesseractservers/Base16Test.java
@@ -0,0 +1,25 @@
+package com.tesseractservers;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Base16Test {
+
+	@Test
+	public void testRandoms() {
+		Random r = new Random();
+		byte[] buf = new byte[16];
+		for (int i = 0; i < 512; i++) {
+			r.nextBytes(buf);
+			String hex = Base16.encode(buf);
+			byte[] buf2 = Base16.decode(hex);
+			String hex2 = Base16.encode(buf2);
+			if (!Arrays.equals(buf, buf2)) {
+				throw new AssertionError("Base16 error:\n- " + hex + "\n- " + hex2);
+			}
+		}
+	}
+
+}
