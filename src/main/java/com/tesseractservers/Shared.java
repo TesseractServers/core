c3214a903407b1d06865010efd4d406f4ac61829
Tassilo <tassia710@gmail.com>
2021-02-06 18:03:00 +0100

    Create Shared.java

---
+++ src/main/java/com/tesseractservers/Shared.java
@@ -0,0 +1,31 @@
+package com.tesseractservers;
+
+import com.tesseractservers.database.Database;
+import net.tassia.event.EventManager;
+
+/**
+ * This class contains static fields which can be accessed by other systems for cleaner code.
+ * @since Core 1.0
+ * @author Tassilo
+ */
+public class Shared {
+
+	/**
+	 * This static field holds a reference to a shared {@link EventManager}.
+	 */
+	public static final EventManager EVENTS = new EventManager();
+
+	/**
+	 * This static field holds a reference to a shared global database connection.
+	 */
+	public static Database DATABASE = null;
+
+
+
+	/**
+	 * Static class.
+	 */
+	private Shared() {
+	}
+
+}
