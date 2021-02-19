52659bc07043d464fc57ed71533a6c846a36efed
Tassilo <tassia710@gmail.com>
2021-02-19 18:54:50 +0100

    Java 2 Kotlin

--- src/main/java/net/proxzi/logging/LogWriter.kt
+++ src/main/java/net/proxzi/logging/LogWriter.kt
@@ -1,17 +1,17 @@
-package net.proxzi.logging;
+package net.proxzi.logging
 
 /**
  * This functional interface is used to write logs to console, files or store them in databases.
- * @since Core 1.0
+ * @since Core 1.1
  * @author Tassilo
  */
 @FunctionalInterface
-public interface LogWriter {
+interface LogWriter {
 
 	/**
 	 * Writes the given log to e.g. a console or a file.
 	 * @param log the log to write
 	 */
-	void writeLog(Log log);
+	fun writeLog(log: Log)
 
 }
