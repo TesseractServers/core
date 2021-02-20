27817822f72e6b7638678791ca5571af709a5f67
Tassilo <tassia710@gmail.com>
2021-02-20 14:18:01 +0100

    Update Comparators.kt

--- src/main/java/net/proxzi/Comparators.kt
+++ src/main/java/net/proxzi/Comparators.kt
@@ -1,7 +1,7 @@
 package net.proxzi
 
 /**
- * Provides utility for {@link Comparator}s.
+ * Provides utility for [Comparator]s.
  * @since Core 1.1
  * @author Tassilo
  */
@@ -9,11 +9,12 @@ object Comparators {
 
 	/**
 	 * Negates/Inverts the comparator.
+	 * @param comparator the comparator to negate
 	 * @param <T> the type
 	 * @return a comparator that behaves the opposite way
 	 */
-	fun <T> Comparator<T>.negate(): Comparator<T> {
-		return Comparator { o1, o2 -> -this.compare(o1, o2) }
+	fun <T> negate(comparator: Comparator<T>): Comparator<T> {
+		return Comparator { o1, o2 -> -comparator.compare(o1, o2) }
 	}
 
 }
